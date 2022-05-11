6fd9b20a055a2048a3b769401bdbeffa6fffdf58
Nonztop <non0824724466@gmail.com>
2022-05-11 20:51:22 +0700

    Final

---
+++ page2.js
@@ -0,0 +1,60 @@
+function check(){
+    var number = document.getElementById("data").value;
+    if(number == 395919){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่1 งวด 16 เมษายน 2565";
+    }
+    else if(number == 737867){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่1 งวด 16 มีนาคม 2565";
+    }
+    else if(number == 970618){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่1 งวด 1 เมษายน 2565";
+    }
+    else if(number == 027641){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 1 เมษายน 2565";
+    }
+    else if(number == 071472){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 1 เมษายน 2565";
+    }
+    else if(number == 240029){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 1 เมษายน 2565";
+    }
+    else if(number == 750408){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 1 เมษายน 2565";
+    }
+    else if(number == 754915){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 1 เมษายน 2565";
+    }
+    else if(number == 120215){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 เมษายน 2565";
+    }
+    else if(number == 288325){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 เมษายน 2565";
+    }
+    else if(number == 345563){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 เมษายน 2565";
+    }
+    else if(number == 691078){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 เมษายน 2565";
+    }
+    else if(number == 189228){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 เมษายน 2565";
+    }
+    else if(number == 324448){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 มีนาคม 2565";
+    }
+    else if(number == 403216){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 มีนาคม 2565";
+    }
+    else if(number == 468367){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 มีนาคม 2565";
+    }
+    else if(number == 757838){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 มีนาคม 2565";
+    }
+    else if(number == 759477){
+        document.getElementById("answer").innerHTML = "คุณถูกรางวัลที่2 งวด 16 มีนาคม 2565";
+    }
+    else{
+        document.getElementById("answer").innerHTML = "คุณไม่ถูกรางวัล";
+    }
+}
